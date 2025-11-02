54f4d441ee9c352428fa9c72570bbcce1ec3db34
Oleg Shilov <shilov-1@yandex.ru>
2025-11-02 13:44:25 +0300

    added filename: "remoteEntry.js" for host;

--- host/webpack.config.js
+++ host/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = (env = {}, argv) => {
   const plugins = [
     new ModuleFederationPlugin({
       name: 'host',
+      filename: 'remoteEntry.js',
       remotes: {
         remote1: 'remote1@http://localhost:3001/remoteEntry.js',
         remote2: 'remote2@http://localhost:3002/remoteEntry.js',
